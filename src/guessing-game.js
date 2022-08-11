af1a8fea75edab266a69b36baf09786e9d9f79d4
Sedric14 <sedricgreen6@gmail.com>
2022-08-11 20:50:51 +0300

    feat: done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,35 @@
 class GuessingGame {
-    constructor() {}
+    // prop = gameMin;
+    // prop = gameMax;
+    // prop = gameGuess;
+    
+    constructor() {
+        this.gameMin = 0;
+        this.gameMax = 0;
+        // this.num = num;
+        this.gameGuess = 0;
+    }
+
+   
 
     setRange(min, max) {
-
+        this.gameMin = min;
+        this.gameMax = max;
+    //    return this.guess(gameMin, gameMax)
     }
 
     guess() {
-
+        console.log(this.gameMax)
+        this.gameGuess =  Math.ceil((this.gameMin -(-this.gameMax))/2);
+return this.gameGuess;
     }
 
     lower() {
-
+        this.gameMax = this.gameGuess;
     }
 
     greater() {
-
+        this.gameMin = this.gameGuess;
     }
 }
 
